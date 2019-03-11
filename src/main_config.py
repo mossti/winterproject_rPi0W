f78d87e47b84f31fdc7628c4171336212e3a3429
mossti <andrewthompson2019@u.northwestern.edu>
2019-03-10 20:39:10 -0500

    added joystick response

--- src/main_config.py
+++ src/main_config.py
@@ -185,7 +185,7 @@ def turn_config():
 
 
 		OAhip0x = hipmid
-		OAknee0x = kneestance + 20
+		OAknee0x = kneestance + 10
 		OAhip1x = hipmid - i*(halfstancestride)
 		OAknee1x = kneestance
 		OAhip2x = OAhip1x - i*(halfcornerstride)
@@ -201,6 +201,7 @@ def turn_config():
 		OAhip7x = OAhip0x
 		OAknee7x = OAknee6x
 		OAhip8x = OAhip0x + i*(halfstancestride)
+		OAknee8x = OAknee7x
 		OAhip9x = OAhip8x + i*(halfcornerstride)
 		OAknee9x = OAknee5x
 		OAhip10x = OAhip9x + i*(halfcornerstride)
@@ -213,7 +214,7 @@ def turn_config():
 		OAknee13x = OAknee0x
 
 		OAhip0xr = hipmid
-		OAknee0xr = kneestance + 20
+		OAknee0xr = kneestance + 10
 		OAhip1xr = hipmid - j*(halfstancestride)
 		OAknee1xr = kneestance
 		OAhip2xr = OAhip1xr - j*(halfcornerstride)
@@ -229,6 +230,7 @@ def turn_config():
 		OAhip7xr = OAhip0xr
 		OAknee7xr = OAknee6xr
 		OAhip8xr = OAhip0xr + j*(halfstancestride)
+		OAknee8xr = OAknee7xr
 		OAhip9xr = OAhip8xr + j*(halfcornerstride)
 		OAknee9xr = OAknee5xr
 		OAhip10xr = OAhip9xr + j*(halfcornerstride)
@@ -240,8 +242,8 @@ def turn_config():
 		OAhip13xr = OAhip8xr
 		OAknee13xr = OAknee0xr
 
-		OAhip0x = hipmid
-		OAknee0v = kneestance - 20
+		OAhip0v = hipmid
+		OAknee0v = kneestance - 10
 		OAhip1v = hipmid - i*(halfstancestride)
 		OAknee1v = kneestance
 		OAhip2v = OAhip1v - i*(halfcornerstride)
@@ -257,6 +259,7 @@ def turn_config():
 		OAhip7v = OAhip0v
 		OAknee7v = OAknee6v
 		OAhip8v = OAhip0v + i*(halfstancestride)
+		OAknee8v = OAknee7v
 		OAhip9v = OAhip8v + i*(halfcornerstride)
 		OAknee9v = OAknee5v
 		OAhip10v = OAhip9v + i*(halfcornerstride)
@@ -269,7 +272,7 @@ def turn_config():
 		OAknee13v = OAknee0v
 
 		OAhip0vr = hipmid
-		OAknee0vr = kneestance - 20
+		OAknee0vr = kneestance - 10
 		OAhip1vr = hipmid - j*(halfstancestride)
 		OAknee1vr = kneestance
 		OAhip2vr = OAhip1vr - j*(halfcornerstride)
@@ -285,6 +288,7 @@ def turn_config():
 		OAhip7vr = OAhip0vr
 		OAknee7vr = OAknee6vr
 		OAhip8vr = OAhip0vr + j*(halfstancestride)
+		OAknee8vr = OAknee7vr
 		OAhip9vr = OAhip8vr + j*(halfcornerstride)
 		OAknee9vr = OAknee5vr
 		OAhip10vr = OAhip9vr + j*(halfcornerstride)
@@ -386,8 +390,9 @@ def turn_config():
 		hip14t = hip0t
 		knee14t = knee0t
 		
-		# stance lists
+		### stance lists
 
+		# normal gait
 		hiplist0 = [hip0, hip1, hip1, hip1, hip1, hip1, hip1, hip1, hip1, hip1, hip2, hip3, hip4, hip5, hip6, hip7, hip8, hip9, hip10, hip11, hip12, hip13]
 		kneelist0 = [knee0, knee1, knee1, knee1, knee1, knee1, knee1, knee1, knee1, knee1, knee2, knee3, knee4, knee5, knee6, knee7, knee8, knee9, knee10, knee11, knee12, knee13]
 		hiplist1 = [hip3, hip4, hip5, hip6, hip7, hip8, hip9, hip10, hip11, hip12, hip13, hip0, hip1, hip1, hip1, hip1, hip1, hip1, hip1, hip1, hip1, hip2]
@@ -397,7 +402,30 @@ def turn_config():
 		kneelist0r = [knee0r, knee1r, knee1r, knee1r, knee1r, knee1r, knee1r, knee1r, knee1r, knee1r, knee2r, knee3r, knee4r, knee5r, knee6r, knee7r, knee8r, knee9r, knee10r, knee11r, knee12r, knee13r]
 		hiplist1r = [hip3r, hip4r, hip5r, hip6r, hip7r, hip8r, hip9r, hip10r, hip11r, hip12r, hip13r, hip0r, hip1r, hip1r, hip1r, hip1r, hip1r, hip1r, hip1r, hip1r, hip1r, hip2r]
 		kneelist1r = [knee3r, knee4r, knee5r, knee6r, knee7r, knee8r, knee9r, knee10r, knee11r, knee12r, knee13r, knee0r, knee1r, knee1r, knee1r, knee1r, knee1r, knee1r, knee1r, knee1r, knee1r, knee2r]
+		
 
+		# on-angle gait
+		# push
+		OAhiplist0x = [OAhip0x, OAhip1x, OAhip1x, OAhip1x, OAhip1x, OAhip1x, OAhip1x, OAhip1x, OAhip1x, OAhip1x, OAhip2x, OAhip3x, OAhip4x, OAhip5x, OAhip6x, OAhip7x, OAhip8x, OAhip9x, OAhip10x, OAhip11x, OAhip12x, OAhip13x]
+		OAkneelist0x = [OAknee0x, OAknee1x, OAknee1x, OAknee1x, OAknee1x, OAknee1x, OAknee1x, OAknee1x, OAknee1x, OAknee1x, OAknee2x, OAknee3x, OAknee4x, OAknee5x, OAknee6x, OAknee7x, OAknee8x, OAknee9x, OAknee10x, OAknee11x, OAknee12x, OAknee13x]
+		OAhiplist1x = [OAhip3x, OAhip4x, OAhip5x, OAhip6x, OAhip7x, OAhip8x, OAhip9x, OAhip10x, OAhip11x, OAhip12x, OAhip13x, OAhip0x, OAhip1x, OAhip1x, OAhip1x, OAhip1x, OAhip1x, OAhip1x, OAhip1x, OAhip1x, OAhip1x, OAhip2x]
+		OAkneelist1x = [OAknee3x, OAknee4x, OAknee5x, OAknee6x, OAknee7x, OAknee8x, OAknee9x, OAknee10x, OAknee11x, OAknee12x, OAknee13x, OAknee0x, OAknee1x, OAknee1x, OAknee1x, OAknee1x, OAknee1x, OAknee1x, OAknee1x, OAknee1x, OAknee1x, OAknee2x]
+
+		OAhiplist0xr = [OAhip0xr, OAhip1xr, OAhip1xr, OAhip1xr, OAhip1xr, OAhip1xr, OAhip1xr, OAhip1xr, OAhip1xr, OAhip1xr, OAhip2xr, OAhip3xr, OAhip4xr, OAhip5xr, OAhip6xr, OAhip7xr, OAhip8xr, OAhip9xr, OAhip10xr, OAhip11xr, OAhip12xr, OAhip13xr]
+		OAkneelist0xr = [OAknee0xr, OAknee1xr, OAknee1xr, OAknee1xr, OAknee1xr, OAknee1xr, OAknee1xr, OAknee1xr, OAknee1xr, OAknee1xr, OAknee2xr, OAknee3xr, OAknee4xr, OAknee5xr, OAknee6xr, OAknee7xr, OAknee8xr, OAknee9xr, OAknee10xr, OAknee11xr, OAknee12xr, OAknee13xr]
+		OAhiplist1xr = [OAhip3xr, OAhip4xr, OAhip5xr, OAhip6xr, OAhip7xr, OAhip8xr, OAhip9xr, OAhip10xr, OAhip11xr, OAhip12xr, OAhip13xr, OAhip0xr, OAhip1xr, OAhip1xr, OAhip1xr, OAhip1xr, OAhip1xr, OAhip1xr, OAhip1xr, OAhip1xr, OAhip1xr, OAhip2xr]
+		OAkneelist1xr = [OAknee3xr, OAknee4xr, OAknee5xr, OAknee6xr, OAknee7xr, OAknee8xr, OAknee9xr, OAknee10xr, OAknee11xr, OAknee12xr, OAknee13xr, OAknee0xr, OAknee1xr, OAknee1xr, OAknee1xr, OAknee1xr, OAknee1xr, OAknee1xr, OAknee1xr, OAknee1xr, OAknee1xr, OAknee2xr]
+		
+		# pull
+		OAhiplist0v = [OAhip0v, OAhip1v, OAhip1v, OAhip1v, OAhip1v, OAhip1v, OAhip1v, OAhip1v, OAhip1v, OAhip1v, OAhip2v, OAhip3v, OAhip4v, OAhip5v, OAhip6v, OAhip7v, OAhip8v, OAhip9v, OAhip10v, OAhip11v, OAhip12v, OAhip13v]
+		OAkneelist0v = [OAknee0v, OAknee1v, OAknee1v, OAknee1v, OAknee1v, OAknee1v, OAknee1v, OAknee1v, OAknee1v, OAknee1v, OAknee2v, OAknee3v, OAknee4v, OAknee5v, OAknee6v, OAknee7v, OAknee8v, OAknee9v, OAknee10v, OAknee11v, OAknee12v, OAknee13v]
+		OAhiplist1v = [OAhip3v, OAhip4v, OAhip5v, OAhip6v, OAhip7v, OAhip8v, OAhip9v, OAhip10v, OAhip11v, OAhip12v, OAhip13v, OAhip0v, OAhip1v, OAhip1v, OAhip1v, OAhip1v, OAhip1v, OAhip1v, OAhip1v, OAhip1v, OAhip1v, OAhip2v]
+		OAkneelist1v = [OAknee3v, OAknee4v, OAknee5v, OAknee6v, OAknee7v, OAknee8v, OAknee9v, OAknee10v, OAknee11v, OAknee12v, OAknee13v, OAknee0v, OAknee1v, OAknee1v, OAknee1v, OAknee1v, OAknee1v, OAknee1v, OAknee1v, OAknee1v, OAknee1v, OAknee2v]
+
+		OAhiplist0vr = [OAhip0vr, OAhip1vr, OAhip1vr, OAhip1vr, OAhip1vr, OAhip1vr, OAhip1vr, OAhip1vr, OAhip1vr, OAhip1vr, OAhip2vr, OAhip3vr, OAhip4vr, OAhip5vr, OAhip6vr, OAhip7vr, OAhip8vr, OAhip9vr, OAhip10vr, OAhip11vr, OAhip12vr, OAhip13vr]
+		OAkneelist0vr = [OAknee0vr, OAknee1vr, OAknee1vr, OAknee1vr, OAknee1vr, OAknee1vr, OAknee1vr, OAknee1vr, OAknee1vr, OAknee1vr, OAknee2vr, OAknee3vr, OAknee4vr, OAknee5vr, OAknee6vr, OAknee7vr, OAknee8vr, OAknee9vr, OAknee10vr, OAknee11vr, OAknee12vr, OAknee13vr]
+		OAhiplist1vr = [OAhip3vr, OAhip4vr, OAhip5vr, OAhip6vr, OAhip7vr, OAhip8vr, OAhip9vr, OAhip10vr, OAhip11vr, OAhip12vr, OAhip13vr, OAhip0vr, OAhip1vr, OAhip1vr, OAhip1vr, OAhip1vr, OAhip1vr, OAhip1vr, OAhip1vr, OAhip1vr, OAhip1vr, OAhip2vr]
+		OAkneelist1vr = [OAknee3vr, OAknee4vr, OAknee5vr, OAknee6vr, OAknee7vr, OAknee8vr, OAknee9vr, OAknee10vr, OAknee11vr, OAknee12vr, OAknee13vr, OAknee0vr, OAknee1vr, OAknee1vr, OAknee1vr, OAknee1vr, OAknee1vr, OAknee1vr, OAknee1vr, OAknee1vr, OAknee1vr, OAknee2vr]
 
 		if direction == 0:
 
@@ -427,33 +455,33 @@ def turn_config():
 		if direction == 1:
 
 
-			hiplist0 = [hip0,(0.5*(hip0+hip1)),hip1,(0.5*(hip1+hip2)),hip2,(0.5*(hip2+hip3)),hip3,(0.5*(hip3+hip0))]
-			kneelist0 = [knee0,(0.5*(knee0+knee1)),knee1,(0.5*(knee1+knee2)),knee2,(0.5*(knee2+knee3)),knee3,(0.5*(knee3+knee0))]
-			hiplist1 = [hip2,(0.5*(hip2+hip3)),hip3,(0.5*(hip3+hip0)),hip0,(0.5*(hip0+hip1)),hip1,(0.5*(hip1+hip2))]
-			kneelist1 = [knee2,(0.5*(knee2+knee3)),knee3,(0.5*(knee3+knee0)),knee0,(0.5*(knee0+knee1)),knee1,(0.5*(knee1+knee2))]
+			#hiplist0 = [hip0,(0.5*(hip0+hip1)),hip1,(0.5*(hip1+hip2)),hip2,(0.5*(hip2+hip3)),hip3,(0.5*(hip3+hip0))]
+			#kneelist0 = [knee0,(0.5*(knee0+knee1)),knee1,(0.5*(knee1+knee2)),knee2,(0.5*(knee2+knee3)),knee3,(0.5*(knee3+knee0))]
+			#hiplist1 = [hip2,(0.5*(hip2+hip3)),hip3,(0.5*(hip3+hip0)),hip0,(0.5*(hip0+hip1)),hip1,(0.5*(hip1+hip2))]
+			#kneelist1 = [knee2,(0.5*(knee2+knee3)),knee3,(0.5*(knee3+knee0)),knee0,(0.5*(knee0+knee1)),knee1,(0.5*(knee1+knee2))]
 		
-			hiplist0r = [hip0r,(0.5*(hip0r+hip1r)),hip1r,(0.5*(hip1r+hip2r)),hip2r,(0.5*(hip2r+hip3r)),hip3r,(0.5*(hip3r+hip0r))]
-			kneelist0r = [knee0r,(0.5*(knee0r+knee1r)),knee1r,(0.5*(knee1r+knee2r)),knee2r,(0.5*(knee2r+knee3r)),knee3r,(0.5*(knee3r+knee0r))]
-			hiplist1r = [hip2r,(0.5*(hip2r+hip3r)),hip3r,(0.5*(hip3r+hip0r)),hip0r,(0.5*(hip0r+hip1r)),hip1r,(0.5*(hip1r+hip2r))]
-			kneelist1r = [knee2r,(0.5*(knee2r+knee3r)),knee3r,(0.5*(knee3r+knee0r)),knee0r,(0.5*(knee0r+knee1r)),knee1r,(0.5*(knee1r+knee2r))]
+			#hiplist0r = [hip0r,(0.5*(hip0r+hip1r)),hip1r,(0.5*(hip1r+hip2r)),hip2r,(0.5*(hip2r+hip3r)),hip3r,(0.5*(hip3r+hip0r))]
+			#kneelist0r = [knee0r,(0.5*(knee0r+knee1r)),knee1r,(0.5*(knee1r+knee2r)),knee2r,(0.5*(knee2r+knee3r)),knee3r,(0.5*(knee3r+knee0r))]
+			#hiplist1r = [hip2r,(0.5*(hip2r+hip3r)),hip3r,(0.5*(hip3r+hip0r)),hip0r,(0.5*(hip0r+hip1r)),hip1r,(0.5*(hip1r+hip2r))]
+			#kneelist1r = [knee2r,(0.5*(knee2r+knee3r)),knee3r,(0.5*(knee3r+knee0r)),knee0r,(0.5*(knee0r+knee1r)),knee1r,(0.5*(knee1r+knee2r))]
 
-			OAhiplist0 = [OAhip0,OAhip1,OAhip2,OAhip3,OAhip4,OAhip5,OAhip6,OAhip7]
-			OAkneelist0 = [OAknee0,OAknee1,OAknee2,OAknee3,OAknee4,OAknee5,OAknee6,OAknee7]
-			OAhiplist1 = [OAhip4,OAhip5,OAhip6,OAhip7,OAhip0,OAhip1,OAhip2,OAhip3]
-			OAkneelist1 = [OAknee4,OAknee5,OAknee6,OAknee7,OAknee0,OAknee1,OAknee2,OAknee3]
+			#OAhiplist0 = [OAhip0,OAhip1,OAhip2,OAhip3,OAhip4,OAhip5,OAhip6,OAhip7]
+			#OAkneelist0 = [OAknee0,OAknee1,OAknee2,OAknee3,OAknee4,OAknee5,OAknee6,OAknee7]
+			#OAhiplist1 = [OAhip4,OAhip5,OAhip6,OAhip7,OAhip0,OAhip1,OAhip2,OAhip3]
+			#OAkneelist1 = [OAknee4,OAknee5,OAknee6,OAknee7,OAknee0,OAknee1,OAknee2,OAknee3]
 		
-			OAhiplist0r = [OAhip0r,OAhip1r,OAhip2r,OAhip3r,OAhip4r,OAhip5r,OAhip6r,OAhip7r]
-			OAkneelist0r = [OAknee0r,OAknee1r,OAknee2r,OAknee3r,OAknee4r,OAknee5r,OAknee6r,OAknee7r]
-			OAhiplist1r = [OAhip4r,OAhip5r,OAhip6r,OAhip7r,OAhip0r,OAhip1r,OAhip2r,OAhip3r]
-			OAkneelist1r = [OAknee4r,OAknee5r,OAknee6r,OAknee7r,OAknee0r,OAknee1r,OAknee2r,OAknee3r]
+			#OAhiplist0r = [OAhip0r,OAhip1r,OAhip2r,OAhip3r,OAhip4r,OAhip5r,OAhip6r,OAhip7r]
+			#OAkneelist0r = [OAknee0r,OAknee1r,OAknee2r,OAknee3r,OAknee4r,OAknee5r,OAknee6r,OAknee7r]
+			#OAhiplist1r = [OAhip4r,OAhip5r,OAhip6r,OAhip7r,OAhip0r,OAhip1r,OAhip2r,OAhip3r]
+			#OAkneelist1r = [OAknee4r,OAknee5r,OAknee6r,OAknee7r,OAknee0r,OAknee1r,OAknee2r,OAknee3r]
 
 			while num<=numturns:
 				j = 0
-				while j<=7:
-					hip_move(0,(hiplist0[j],hiplist0r[j],OAhiplist0[j]))
-					knee_move(0,(kneelist0[j],kneelist0r[j],OAkneelist0[j]))
-					hip_move(1,(OAhiplist1[j],hiplist1r[j],hiplist1[j]))
-					knee_move(1,(OAkneelist1[j],kneelist1[j],kneelist1[j]))
+				while j<=21:
+					hip_move(0,(hiplist0[j],hiplist0r[j],OAhiplist0v[j]))
+					knee_move(0,(kneelist0[j],kneelist0r[j],OAkneelist0v[j]))
+					hip_move(1,(OAhiplist1x[j],hiplist1r[j],hiplist1[j]))
+					knee_move(1,(OAkneelist1x[j],kneelist1[j],kneelist1[j]))
 					time.sleep(velocity)
 					j+=1
 
@@ -461,33 +489,33 @@ def turn_config():
 		if direction == 3:
 
 
-			hiplist0 = [hip0,(0.5*(hip0+hip1)),hip1,(0.5*(hip1+hip2)),hip2,(0.5*(hip2+hip3)),hip3,(0.5*(hip3+hip0))]
-			kneelist0 = [knee0,(0.5*(knee0+knee1)),knee1,(0.5*(knee1+knee2)),knee2,(0.5*(knee2+knee3)),knee3,(0.5*(knee3+knee0))]
-			hiplist1 = [hip2,(0.5*(hip2+hip3)),hip3,(0.5*(hip3+hip0)),hip0,(0.5*(hip0+hip1)),hip1,(0.5*(hip1+hip2))]
-			kneelist1 = [knee2,(0.5*(knee2+knee3)),knee3,(0.5*(knee3+knee0)),knee0,(0.5*(knee0+knee1)),knee1,(0.5*(knee1+knee2))]
+			#hiplist0 = [hip0,(0.5*(hip0+hip1)),hip1,(0.5*(hip1+hip2)),hip2,(0.5*(hip2+hip3)),hip3,(0.5*(hip3+hip0))]
+			#kneelist0 = [knee0,(0.5*(knee0+knee1)),knee1,(0.5*(knee1+knee2)),knee2,(0.5*(knee2+knee3)),knee3,(0.5*(knee3+knee0))]
+			#hiplist1 = [hip2,(0.5*(hip2+hip3)),hip3,(0.5*(hip3+hip0)),hip0,(0.5*(hip0+hip1)),hip1,(0.5*(hip1+hip2))]
+			#kneelist1 = [knee2,(0.5*(knee2+knee3)),knee3,(0.5*(knee3+knee0)),knee0,(0.5*(knee0+knee1)),knee1,(0.5*(knee1+knee2))]
 		
-			hiplist0r = [hip0r,(0.5*(hip0r+hip1r)),hip1r,(0.5*(hip1r+hip2r)),hip2r,(0.5*(hip2r+hip3r)),hip3r,(0.5*(hip3r+hip0r))]
-			kneelist0r = [knee0r,(0.5*(knee0r+knee1r)),knee1r,(0.5*(knee1r+knee2r)),knee2r,(0.5*(knee2r+knee3r)),knee3r,(0.5*(knee3r+knee0r))]
-			hiplist1r = [hip2r,(0.5*(hip2r+hip3r)),hip3r,(0.5*(hip3r+hip0r)),hip0r,(0.5*(hip0r+hip1r)),hip1r,(0.5*(hip1r+hip2r))]
-			kneelist1r = [knee2r,(0.5*(knee2r+knee3r)),knee3r,(0.5*(knee3r+knee0r)),knee0r,(0.5*(knee0r+knee1r)),knee1r,(0.5*(knee1r+knee2r))]
+			#hiplist0r = [hip0r,(0.5*(hip0r+hip1r)),hip1r,(0.5*(hip1r+hip2r)),hip2r,(0.5*(hip2r+hip3r)),hip3r,(0.5*(hip3r+hip0r))]
+			#kneelist0r = [knee0r,(0.5*(knee0r+knee1r)),knee1r,(0.5*(knee1r+knee2r)),knee2r,(0.5*(knee2r+knee3r)),knee3r,(0.5*(knee3r+knee0r))]
+			#hiplist1r = [hip2r,(0.5*(hip2r+hip3r)),hip3r,(0.5*(hip3r+hip0r)),hip0r,(0.5*(hip0r+hip1r)),hip1r,(0.5*(hip1r+hip2r))]
+			#kneelist1r = [knee2r,(0.5*(knee2r+knee3r)),knee3r,(0.5*(knee3r+knee0r)),knee0r,(0.5*(knee0r+knee1r)),knee1r,(0.5*(knee1r+knee2r))]
 
-			OAhiplist0 = [OAhip0,OAhip1,OAhip2,OAhip3,OAhip4,OAhip5,OAhip6,OAhip7]
-			OAkneelist0 = [OAknee0,OAknee1,OAknee2,OAknee3,OAknee4,OAknee5,OAknee6,OAknee7]
-			OAhiplist1 = [OAhip4,OAhip5,OAhip6,OAhip7,OAhip0,OAhip1,OAhip2,OAhip3]
-			OAkneelist1 = [OAknee4,OAknee5,OAknee6,OAknee7,OAknee0,OAknee1,OAknee2,OAknee3]
+			#OAhiplist0 = [OAhip0,OAhip1,OAhip2,OAhip3,OAhip4,OAhip5,OAhip6,OAhip7]
+			#OAkneelist0 = [OAknee0,OAknee1,OAknee2,OAknee3,OAknee4,OAknee5,OAknee6,OAknee7]
+			#OAhiplist1 = [OAhip4,OAhip5,OAhip6,OAhip7,OAhip0,OAhip1,OAhip2,OAhip3]
+			#OAkneelist1 = [OAknee4,OAknee5,OAknee6,OAknee7,OAknee0,OAknee1,OAknee2,OAknee3]
 		
-			OAhiplist0r = [OAhip0r,OAhip1r,OAhip2r,OAhip3r,OAhip4r,OAhip5r,OAhip6r,OAhip7r]
-			OAkneelist0r = [OAknee0r,OAknee1r,OAknee2r,OAknee3r,OAknee4r,OAknee5r,OAknee6r,OAknee7r]
-			OAhiplist1r = [OAhip4r,OAhip5r,OAhip6r,OAhip7r,OAhip0r,OAhip1r,OAhip2r,OAhip3r]
-			OAkneelist1r = [OAknee4r,OAknee5r,OAknee6r,OAknee7r,OAknee0r,OAknee1r,OAknee2r,OAknee3r]
+			#OAhiplist0r = [OAhip0r,OAhip1r,OAhip2r,OAhip3r,OAhip4r,OAhip5r,OAhip6r,OAhip7r]
+			#OAkneelist0r = [OAknee0r,OAknee1r,OAknee2r,OAknee3r,OAknee4r,OAknee5r,OAknee6r,OAknee7r]
+			#OAhiplist1r = [OAhip4r,OAhip5r,OAhip6r,OAhip7r,OAhip0r,OAhip1r,OAhip2r,OAhip3r]
+			#OAkneelist1r = [OAknee4r,OAknee5r,OAknee6r,OAknee7r,OAknee0r,OAknee1r,OAknee2r,OAknee3r]
 
 			while num<=numturns:
 				j = 0
-				while j<=7:
-					hip_move(0,(hiplist0r[j],hiplist0[j],OAhiplist0[j]))
-					knee_move(0,(kneelist0r[j],kneelist0[j],OAkneelist0[j]))
-					hip_move(1,(OAhiplist1[j],hiplist1[j],hiplist1r[j]))
-					knee_move(1,(OAkneelist1[j],kneelist1r[j],kneelist1r[j]))
+				while j<=21:
+					hip_move(0,(hiplist0r[j],hiplist0[j],OAhiplist0x[j]))
+					knee_move(0,(kneelist0r[j],kneelist0[j],OAkneelist0x[j]))
+					hip_move(1,(OAhiplist1v[j],hiplist1[j],hiplist1r[j]))
+					knee_move(1,(OAkneelist1v[j],kneelist1r[j],kneelist1r[j]))
 					time.sleep(velocity)
 					j+=1
 
--- src/read_input.py
+++ src/read_input.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python
 
+import roslib
 import rospy
-from std_msgs.msg import String
+import std_msgs.msg
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
+from ServoPi import PWM
+import time
+import math
 
+pwm = PWM(0x40)
+pwm.set_pwm_freq(50)
+
+ts = 0.2
+
+low = 205
+mid = 307
+high = 410
 
 def callback(data):
 	rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.buttons)
-
+	if data.buttons[0] == 1:
+		pwm.set_pwm(1, 0, mid)
+	if data.buttons[3] == 1:
+		pwm.set_pwm(1, 0, low)
+	if data.buttons[1] == 1:
+		pwm.set_pwm(1, 0, high)
 
 def listener():
 
